8add57462c6b6da2227cec88e2ce53b862ad874b
gayeon <lgy3131@naver.com>
2026-02-17 20:30:03 +0900

    style : ConfirmModal 전역 스타일 적용

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -13,13 +13,13 @@ const ConfirmModal = ({
 }: ConfirmModalProps) => {
   return (
     <div className="absolute flex inset-0 z-60 bg-black/70 items-center justify-center backdrop-blur-md">
-      <div className="flex flex-col bg-white p-5 gap-4 rounded-xl">
+      <div className="flex flex-col bg-common-white px-5 pt-5 pb-4 gap-4 rounded-xl w-90">
         <div className="flex flex-col gap-2">
           <div className="text-sub1_sb_18">{title}</div>
-          <div>{detail}</div>
+          <div className="text-body4_r_14 text-neutral-30">{detail}</div>
         </div>
-        <div className="flex items-center justify-end">
-          <button onClick={onClose} className="w-20 px-1 py-2">
+        <div className="flex items-center justify-end text-sub4_sb_14">
+          <button onClick={onClose} className="w-15 px-1 py-2 text-neutral-50">
             취소
           </button>
           <button onClick={onConfirm} className="w-20 px-1 py-2 text-primary">
