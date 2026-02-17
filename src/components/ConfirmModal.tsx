862ae07dd957b1fbb586108a4d6231e91bdaf9e2
gayeon <lgy3131@naver.com>
2026-02-17 20:08:05 +0900

    style : 전역 Pretendard 폰트 설정

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -15,17 +15,14 @@ const ConfirmModal = ({
     <div className="absolute flex inset-0 z-60 bg-black/70 items-center justify-center backdrop-blur-md">
       <div className="flex flex-col bg-white p-5 gap-4 rounded-xl">
         <div className="flex flex-col gap-2">
-          <div>{title}</div>
+          <div className="text-sub1_sb_18">{title}</div>
           <div>{detail}</div>
         </div>
         <div className="flex items-center justify-end">
           <button onClick={onClose} className="w-20 px-1 py-2">
             취소
           </button>
-          <button
-            onCanPlayThrough={onConfirm}
-            className="w-20 px-1 py-2 text-primary"
-          >
+          <button onClick={onConfirm} className="w-20 px-1 py-2 text-primary">
             확인
           </button>
         </div>
