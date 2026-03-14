840fa6d7d98c6f1e0ee382ace59fa3a951377952
gayeon <lgy3131@naver.com>
2026-03-14 21:39:39 +0900

    style : DisposalComplete 페이지 레아아웃 수정

--- src/pages/Walk/DisposalComplete.tsx
+++ src/pages/Walk/DisposalComplete.tsx
@@ -1,4 +1,5 @@
-import { FaCircleCheck } from "react-icons/fa6";
+import CheckIcon from "../../assets/icons/check_fill.svg?react";
+import BulletIcon from "../../assets/icons/bullet.svg?react";
 import { useNavigate } from "react-router-dom";
 
 const DisposalComplete = () => {
@@ -10,44 +11,50 @@ const DisposalComplete = () => {
 
   return (
     <div className="bg-background inset-0 flex flex-col items-center px-5">
-      <div className="flex flex-col gap-5 mt-60">
+      <div className="flex flex-col gap-4 mt-58.5">
         <div className="flex justify-center">
-          <FaCircleCheck className="text-primary size-15" />
+          <CheckIcon className="text-primary w-14 h-14" />
         </div>
         <div className="text-head1_sb_24 text-center">
           폐의약품 수거 인증이 <br />
           완료되었어요
         </div>
       </div>
-      <div className="flex w-full flex-col gap-3.5 mt-40">
-        <div className="flex justify-between px-4 py-3 bg-primary-extralight border border-primary-light rounded-xl items-center">
-          <div className="text-primary flex gap-1.5 text-sub3_sb_16 items-center">
-            <FaCircleCheck className="size-4" />
+      <div className="flex w-full flex-col gap-3 mt-35">
+        <div className="flex justify-between p-3 bg-primary-extralight border border-primary-light rounded-lg items-center">
+          <div className="text-primary flex gap-0.5 text-sub3_sb_16 items-center">
+            <CheckIcon className="w-5 h-5" />
             800원 적립 완료
           </div>
           <div className="text-caption1-m-13 text-cool-neutral-30">
             누적 잔액 50,850원
           </div>
         </div>
-        <div className="flex flex-col p-4 bg-common-white shadow-md shadow-[#4a4e56]/4 rounded-xl gap-4.5 text-body3_r_16">
+        <div className="flex flex-col py-4 pl-3 pr-4 bg-common-white shadow-card rounded-lg gap-3 text-body3_r_16">
           <div className="flex justify-between">
-            <div>• 오늘의 운동</div>
+            <div className="flex gap-0.5 items-center">
+              <BulletIcon className="w-4 h-4 text-neutral-30" />
+              <span className="text-common-black">오늘의 운동</span>
+            </div>
             <div>618m 산책 완료</div>
           </div>
           <div className="flex justify-between">
-            <div>• 오늘의 성취</div>
+            <div className="flex gap-0.5 items-center">
+              <BulletIcon className="w-4 h-4 text-neutral-30" />
+              <span className="text-common-black">오늘의 성취</span>
+            </div>
             <div>초보 환경 지킴이 달성</div>
           </div>
         </div>
       </div>
-      <div className="flex gap-2 w-full text-sub3_sb_16 mt-15">
+      <div className="flex gap-2 w-full text-sub3_sb_16 mt-12">
         <button
           onClick={returnHome}
-          className="flex-1 py-4 bg-common-white text-primary border border-primary rounded-xl"
+          className="flex-1 py-4 bg-common-white text-primary border border-primary rounded-lg"
         >
           홈 화면으로
         </button>
-        <button className="flex-1 py-4 bg-primary text-common-white rounded-xl">
+        <button className="flex-1 py-4 bg-primary text-common-white rounded-lg">
           리워드 환급받기
         </button>
       </div>
