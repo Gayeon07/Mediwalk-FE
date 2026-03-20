08243250440d76986f2b52c88ac68c7be12a5b1a
gayeon <lgy3131@naver.com>
2026-03-21 02:09:07 +0900

    feat : Badge 컴포넌트 구현 및 적용

--- src/pages/Mypage/SettingListItem.tsx
+++ src/pages/Mypage/SettingListItem.tsx
@@ -7,6 +7,7 @@ import SettingIcon from "../../assets/icons/setting_fill.svg?react";
 import InfoIcon from "../../assets/icons/info_fill.svg?react";
 import QuestionIcon from "../../assets/icons/question_fill.svg?react";
 import ArrowIcon from "../../assets/icons/arrow2_right.svg?react";
+import Badge from "../../components/Badge";
 
 const ICON_COMPONENTS = {
   person: PersonIcon,
@@ -32,7 +33,11 @@ const SettingListItem = ({ icon, title }: SettingListItemProps) => {
     <div className="flex justify-between items-center bg-common-white py-3">
       <div className="flex gap-2 items-center">
         <IconComponent className="w-5 h-5 text-cool-neutral-60" />
-        <span className="text-body1_m_16">{title}</span>
+        <div className="flex items-center gap-1">
+          <span className="text-body1_m_16">{title}</span>
+          {/* icon 문자열이 "account"일 때만 Badge 렌더링 */}
+          {icon === "account" && <Badge text="환급용" />}
+        </div>
       </div>
       <ArrowIcon className="w-5 h-5 text-cool-neutral-30" />
     </div>
