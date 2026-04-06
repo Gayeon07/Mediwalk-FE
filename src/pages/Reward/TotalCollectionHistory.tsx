0b86b1c12eae842f2b8ec759a51e0e3c12ef972a
gayeon <lgy3131@naver.com>
2026-04-07 00:27:16 +0900

    feat : TotalCollectionHistory 페이지 UI 구현

--- src/pages/Reward/TotalCollectionHistory.tsx
+++ src/pages/Reward/TotalCollectionHistory.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
 import ArrowIcon from "../../assets/icons/arrow1_left.svg?react";
+import BulletIcon from "../../assets/icons/bullet.svg?react";
 
 const TotalCollectionHistory = () => {
   const navigate = useNavigate();
@@ -9,7 +10,52 @@ const TotalCollectionHistory = () => {
       <header className="pt-6 pb-3 items-baseline shrink-0">
         <ArrowIcon className="w-6 h-6" onClick={() => navigate("/reward")} />
       </header>
-      <main></main>
+      <main className="flex flex-col gap-6 pt-6 pb-15">
+        <section className="flex flex-col gap-1">
+          <div className="flex gap-1 text-head1_sb_24">
+            누적 수거<span className="text-primary">18회</span>
+          </div>
+          <div className="text-body4_r_14 text-cool-neutral-40">
+            이번 년도 누적 폐의약품 수거 횟수를 알려드려요
+          </div>
+        </section>
+        <section className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2">
+            <article className="flex gap-2.5 p-4 rounded-xl bg-[#EDF3FF] border-[1.5px] border-primary">
+              <div className="w-11 h-11 bg-[#97A2B8] rounded-md"></div>
+              <div className="flex flex-col flex-1 gap-1">
+                <div className="flex justify-between items-center">
+                  <div className="text-[#31353B] text-sub3_sb_16">
+                    폐의약품 수거
+                  </div>
+                  <div className="text-primary text-sub4_sb_14">+ 3,000원</div>
+                </div>
+                <div className="flex gap-0.5 items-center text-[#62738F] text-caption3_r_13">
+                  2026. 01. 01
+                  <BulletIcon className="w-4 h-4" />
+                  강남구 보건소
+                </div>
+              </div>
+            </article>
+            <article className="flex gap-2.5 p-4 rounded-xl bg-common-white shadow-card">
+              <div className="w-11 h-11 bg-[#97A2B8] rounded-md"></div>
+              <div className="flex flex-col flex-1 gap-1">
+                <div className="flex justify-between items-center">
+                  <div className="text-[#31353B] text-sub3_sb_16">
+                    폐의약품 수거
+                  </div>
+                  <div className="text-primary text-sub4_sb_14">+ 0원</div>
+                </div>
+                <div className="flex gap-0.5 items-center text-[#62738F] text-caption3_r_13">
+                  2026. 01. 01
+                  <BulletIcon className="w-4 h-4" />
+                  강남구 보건소
+                </div>
+              </div>
+            </article>
+          </div>
+        </section>
+      </main>
     </div>
   );
 };
